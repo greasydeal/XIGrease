1ab34425f978a69fb38d4195486769b18faf78da
greasydeal <waylon_brown@hotmail.com>
2022-01-14 17:07:30 -0600

    init

---
+++ XIGrease.lua
@@ -0,0 +1,18 @@
+_addon.name = 'XIGrease'
+_addon.author = 'greasydeal'
+_addon.version = 'beta 0.1'
+_addon_description = 'Connects player to monitoring services.'
+
+local socket = require("socket")
+
+local port = 33420
+local ip = "74.206.40.95"
+local tcp = assert(socket.tcp())
+
+ashita.register_event('load', function()
+    if (tcp:connect(ip, port) == 1) then
+        print('\31\200[\31\05XIGrease\31\200]\31\207 '.. "Connected to XIGrease network!")
+    else
+        print('\31\200[\31\05XIGrease\31\200]\31\207 '.. "ERROR: Could not connect to XIGrease network!")
+    end;
+end);
